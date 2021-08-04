489515c2aef3344f17ed88cf24fcb7329a78b152
blessnathan01 <blessingnathaniel01@gmail.com>
2021-08-05 00:53:48 +0300

    the very first pytask was added

---
+++ pytasks/pyeasy.py
@@ -0,0 +1,24 @@
+# print hello world
+print('Hello World')
+
+# print a triangle (order of instructions matters a lot)
+print('\n')
+print('   /|')
+print('  / |')
+print(' /  |')
+print('/___|')
+print('\n')
+
+# variables and data types (strings, integers, floating point numbers and boolean)
+character_name = 'Thomas'
+character_age = '53'
+print('There once a man named ' + character_name + ', ')
+print('he was ' + character_age + ' years old.')
+print('He really liked the name ' + character_name + ', ')
+print('but didn\'t like being ' + character_age + '.')
+print('\n')
+
+# working with strings (plain text) and built-in functions (lower, upper, islower, isupper, len, index, replace)
+pyvar = 'pynathan version 1'
+print(pyvar.upper() + ' is out, check it now')
+
